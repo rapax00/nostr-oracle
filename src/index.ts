573482c89dca3594e974dcfd0d8afe75f669f380
Rapax <gasparpelayo44@gmail.com>
2024-09-11 18:14:28 -0300

    fix: use ndk instance

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,21 @@
-import NDK, { NDKPrivateKeySigner } from '@nostr-dev-kit/ndk';
+import 'websocket-polyfill';
 import { getBitcoinPrice } from './services/yadio';
 import { makeEvent } from './utils/nostrEvent';
+import { ndkClient } from './services/ndk';
 
 const start = async () => {
     try {
-        const signer = new NDKPrivateKeySigner(process.env.NOSTR_PRIVATE_KEY!);
+        // NDK
+        await ndkClient.init();
 
-        const ndk = new NDK({
-            explicitRelayUrls: [
-                'wss://nos.lol',
-                'wss://nostr-pub.wellorder.net',
-                'wss://relay.damus.io',
-                'wss://relay.hodl.ar',
-            ],
-            signer,
-        });
+        const ndk = ndkClient.getInstance();
 
-        ndk.connect();
-
-        console.log('ready'); // debug
-
+        // Make Event
         const price = await getBitcoinPrice('usd');
 
         const event = await makeEvent('btc:price', price.toString(), ndk);
 
-        console.log('event: ', await event.toNostrEvent()); // debug
-        console.log('relays', ndk!.explicitRelayUrls); // debug
-
+        // Publish Event
         await event.publish();
     } catch (error) {
         console.error('Error to publish event', error);
