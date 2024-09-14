02fd3385e240906f6195ffbd038f16883136da30
Rapax <gasparpelayo44@gmail.com>
2024-09-14 20:30:33 -0300

    fix: rename

---
+++ src/utils/makeNostrEvent.ts
@@ -0,0 +1,28 @@
+import { EventTemplate, finalizeEvent, NostrEvent } from 'nostr-tools';
+
+async function makeNostrEvent(
+    dTagValue: string,
+    content: string,
+    privateKey: Uint8Array
+): Promise<NostrEvent> {
+    try {
+        const unsignedEvent: EventTemplate = {
+            kind: 30021,
+            tags: [['d', dTagValue]],
+            content: JSON.stringify(content),
+            created_at: Math.round(Date.now() / 1000),
+        };
+
+        const signedEvent: NostrEvent = finalizeEvent(
+            unsignedEvent,
+            privateKey
+        );
+
+        return signedEvent;
+    } catch (error) {
+        console.log('Error in makeNostrEvent.ts: ' + error);
+        throw error;
+    }
+}
+
+export { makeNostrEvent };
