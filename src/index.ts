10224abe4a4270e049f1d576b6eb804391377b9a
Rapax <gasparpelayo44@gmail.com>
2024-09-14 20:51:36 -0300

    fix: use new schema

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,39 @@
 import 'websocket-polyfill';
+import * as dotenv from 'dotenv';
 import { getBitcoinPrice } from './services/yadio';
-import { makeEvent } from './utils/nostrEvent';
-import { ndkClient } from './services/ndk';
+import { makeNostrEvent } from './utils/makeNostrEvent';
+import { generateRelay } from './services/relay';
+
+dotenv.config();
 
 const start = async () => {
     try {
-        // NDK
-        await ndkClient.init();
+        // Key
+        const privateKey = Uint8Array.from(
+            Buffer.from(process.env.NOSTR_PRIVATE_KEY!, 'hex')
+        );
 
-        const ndk = ndkClient.getInstance();
+        // Relay
+        const relayUrl = 'wss://relay.damus.io';
+
+        const relay = await generateRelay(relayUrl);
 
         // Make Event
         const price = await getBitcoinPrice('usd');
 
-        const event = await makeEvent('btc:price', price.toString(), ndk);
+        const event = await makeNostrEvent(
+            'btc:price',
+            price.toString(),
+            privateKey
+        );
 
         // Publish Event
-        await event.publish();
+        console.log('Publishing event:', event);
+
+        await relay.publish(event);
     } catch (error) {
         console.error('Error to publish event', error);
     }
 };
 
-start();
+setInterval(start, 5000);
